72e7a1cbfc25bed958eea561b833f30cc98e8270
ArgaJR <athillayusuf06@gmail.com>
2024-11-28 19:01:08 +0700

    second commit

---
+++ src/component/card/breadcrumb/breadcrumb.type.tsx
@@ -0,0 +1,12 @@
+export type BreadcrumbVariant = 'small' | 'medium' | 'large';
+
+export interface BreadcrumbData {
+  label: string;
+  href?: string;
+}
+
+export interface BreadcrumbProps {
+  variant?: BreadcrumbVariant;
+  datas: BreadcrumbData[];
+  spacer?: React.ReactNode; // Custom spacer
+}
---
+++ src/component/card/toast/toast.type.tsx
@@ -0,0 +1,13 @@
+export type ToastType = 'information' | 'notification';
+
+export type ToastVariant = 'success' | 'warning' | 'danger' | 'default';
+
+export type ToastSize = 'small' | 'medium' | 'large';
+
+export interface ToastProps {
+  type?: ToastType;
+  variant?: ToastVariant;
+  size?: ToastSize;
+  message: string;
+  onClick?: () => void; // Click handler
+}
